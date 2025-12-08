5ba660b74ac3f54e63d9766cccda616a3acf0892
Abhishek Tarun <abhishek.tarun09@gmail.com>
2025-12-08 16:00:02 +0100

    Data Analysis to solve business problems

---
+++ src/ingestion/ingestion_to_db.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os
+import time
+import sys
+
+from src.logger import logging
+from src.exception import CustomException
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///inventory.db')
+
+def ingest_db(df, table_name, engine):
+    """This function ingests data into SQL Database"""
+    
+    try:
+        df.to_sql(table_name, con = engine, if_exists = 'replace', index = False)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_raw_data():    
+    """This function will load CSV files as pandas dataframe and ingest into db"""
+    
+    try:
+        start_time = time.time()
+        for file in os.listdir('data'):
+            if '.csv' in file:
+                df = pd.read_csv('data/'+file)
+                logging.info(f"Ingesting {file} in db.")
+                ingest_db(df, file[:-4], engine)
+        end_time = time.time()
+        logging.info("----------Ingestion Completed----------")
+        total_time = (end_time - start_time)/60
+        logging.info(f"Time taken {total_time} minutes.")
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+if __name__ == '__main__':
+    load_raw_data()
